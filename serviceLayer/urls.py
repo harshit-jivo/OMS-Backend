d4788fad45bca5e7c1618404449e3f40b3abf0e7
Harshit Singh <harshit62512@gmail.com>
2026-06-18 16:54:48 +0530

    Refactor draft handling: replace DraftCreateView with DraftView and enhance SAP session management in SAPInvoiceCreateView

--- serviceLayer/urls.py
+++ serviceLayer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import SAPInvoiceCreateView , DraftCreateView , DraftActionView
+from .views import SAPInvoiceCreateView , DraftView , DraftActionView 
 
 urlpatterns = [
     path('invoice/' , SAPInvoiceCreateView.as_view()),
-    path('draft/' , DraftCreateView.as_view()),
+    path('draft/' , DraftView.as_view()),
     path('draft-action/' , DraftActionView.as_view())
 
 ]
